ab5309fba90a4eb4194acef4e75b4770684bd93c
Riccardo Rossetti <riccardo.rossetti@reactconsulting.it>
2022-03-10 12:53:25 +0100

    Modifica all'API per prova fetch del JS (dopo modifica dal site)

--- VAR/Server/Controllers/WeatherForecastController.cs
+++ VAR/Server/Controllers/WeatherForecastController.cs
@@ -23,7 +23,7 @@ namespace VAR.Server.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 120).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
